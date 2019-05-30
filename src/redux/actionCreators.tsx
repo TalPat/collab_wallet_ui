4301535d602de766b9149efc96315f2e12dd0797
revan-wy <revan-wy@student.wethinkcode.co.za>
2019-05-30 13:25:44 +0200

    added new actions

--- src/redux/actionCreators.tsx
+++ src/redux/actionCreators.tsx
@@ -1,23 +1,68 @@
+import { number } from "prop-types";
+
+interface IUser {
+  id: number,
+  userName: string,
+  role: string,
+  dateCreated: string,
+  deletedAt: string,
+}
+
+interface IAccount {
+  id: number,
+  name: string,
+  owner: number,
+  deletedAt: string,
+  lastUpdate: string,
+}
+
+interface ITransaction {
+  id: number,
+  debitAccountID: number,
+  creditAccountID: number,
+  amount: number,
+  date: string,
+}
+
 export interface IAction {
-  type: string,
   payload: {
     accountID?: number,
     token?: string,
     userID?: number,
-  }
+    arrayOfUsers?: IUser[],
+    arrayOfAccounts?: IAccount[],
+    arrayOfTransactions?: ITransaction[],
+    view?: string,
+  },
+  type: string,
 }
 
-export function adminLogin(userID: number, token: string) {
+export function adminLogin(token: string): IAction {
   return {
     payload: {
       token,
-      userID,
     },
     type: 'ADMIN_LOGIN',
   }
 }
 
-export function selectUser(userID: number) {
+export function adminLogout(): IAction {
+  return {
+    payload: {},
+    type: 'ADMIN_LOGIN',
+  }
+}
+
+export function updateUsersList(arrayOfUsers: IUser[]): IAction {
+  return {
+    payload: {
+      arrayOfUsers,
+    },
+    type: 'UPDATE_USERS_LIST',
+  }
+}
+
+export function selectUser(userID: number): IAction {
   return {
     payload: {
       userID,
@@ -26,11 +71,38 @@ export function selectUser(userID: number) {
   }
 }
 
-export function selectAccount(accountID: number) {
+export function updateAccountsList(arrayOfAccounts: IAccount[]): IAction {
+  return {
+    payload: {
+      arrayOfAccounts,
+    },
+    type: 'UPDATE_ACCOUNTS_LIST',
+  }
+}
+
+export function selectAccount(accountID: number): IAction {
   return {
     payload: {
       accountID,
     },
     type: 'SELECT_ACCOUNT',
+  }
+}
+
+export function updateTransactionsList(arrayOfTransactions: ITransaction[]): IAction {
+  return {
+    payload: {
+      arrayOfTransactions,
+    },
+    type: 'UPDATE_TRANSACTIONS_LIST',
+  }
+}
+
+export function selectActiveView(newView: string): IAction {
+  return {
+    payload: {
+      view: newView,
+    },
+    type: 'SELECT_ACTIVE_VIEW',
   }
 }
